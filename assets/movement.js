8cd764476e1d0afa624ca98f3b8986a1c45597a0
Zaitonn <103164490+Zaitonn@users.noreply.github.com>
2023-03-18 20:08:37 +0800

    docs: init

---
+++ assets/movement.js
@@ -0,0 +1,3 @@
+window.addEventListener('mousemove', (ev) => document.querySelectorAll('.movement-follow-mouse').forEach((el) => el.setAttribute('style', `transform: scale(1.01) translate(${(ev.x - window.innerWidth / 2) / window.innerWidth * 10}px,${(ev.y - window.innerHeight / 2) / window.innerHeight * 10}px)`)));
+window.addEventListener('mouseout', () => document.querySelectorAll('.movement-follow-mouse').forEach((el) => el.setAttribute('style', 'transform: scale(1.005)')));
+window.addEventListener('mouseenter', () => document.querySelectorAll('.movement-follow-mouse').forEach((el) => el.setAttribute('style', 'transform: scale(1.01)')));
